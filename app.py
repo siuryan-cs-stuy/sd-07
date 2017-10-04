53765fe298e8b13d2fbe348d9d18c54930eddff1
Ryan Siu <rsiu@stuy.edu>
2017-10-04 14:34:48 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, flash, session
+
+app = Flask(__name__)
+app.secret_key = "SECRET KEY"
+
+USERNAME = 'test'
+PASSWORD = 'pwd'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if 'username' in session:
+        return redirect('/welcome')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == USERNAME:
+            if password == PASSWORD:
+                session['username'] = username
+                return redirect('/welcome')
+            else:
+                return render_template('index.html', error = 'password')
+        else:
+            return render_template('index.html', error = 'username')
+        
+    return render_template('index.html')
+
+@app.route('/welcome')
+def welcome():
+    if 'username' in session:
+        return render_template('welcome.html', username = session['username'])
+    else:
+        return redirect('/')
+    
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
