4fd27b1ed214f1fdbe51f99952ffdf9d29183712
Ryan Siu <rsiu@stuy.edu>
2017-10-11 14:32:21 -0400

    Add flash

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template, request, redirect, flash, session
+from flask import Flask, render_template, request, redirect, flash, session, url_for
 
 app = Flask(__name__)
 app.secret_key = "SECRET KEY"
 
 USERNAME = 'test'
 PASSWORD = 'pwd'
+
+# Returns 0 for success, 1 for incorrect user, 2 for incorrect password
+def auth(input_user, input_pass):
+    if input_user == USERNAME:
+       if input_pass == PASSWORD:
+           session['username'] = input_user
+           flash('Logged in')
+           return 0
+       else:
+           flash('Incorrect password')
+           return 2
+    else:
+        flash('Incorrect username')
+        return 1
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,15 +27,8 @@ def index():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username == USERNAME:
-            if password == PASSWORD:
-                session['username'] = username
-                return redirect('/welcome')
-            else:
-                return render_template('index.html', error = 'password')
-        else:
-            return render_template('index.html', error = 'username')
-        
+        if auth(username, password) == 0:
+            return redirect(url_for('welcome'))
     return render_template('index.html')
 
 @app.route('/welcome')
@@ -29,11 +36,13 @@ def welcome():
     if 'username' in session:
         return render_template('welcome.html', username = session['username'])
     else:
+        flash('You need to be logged in')
         return redirect(url_for('index'))
     
 @app.route('/logout')
 def logout():
     session.pop('username')
+    flash('Logged out')
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
